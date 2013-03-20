31ab2913c1f6b3305f1b3048c7f53b71a237aa70
msuarz <mikemps@gmail.com>
2013-03-20 11:51:36 -0400

    setting up knife/chef server

--- .chef/knife.rb
+++
@@ -1,8 +0,0 @@
-log_level                :info
-log_location             STDOUT
-node_name                'msuarz'
-client_key               'c:/dev/openstack/.chef/msuarz.pem'
-validation_client_name   'chef-validator'
-validation_key           'C:/dev/openstack/.chef/validation.pem'
-chef_server_url          'http://precise64'
-syntax_check_cache_path  'c:/dev/openstack/.chef/syntax_check_cache'
---
+++ chef/knife.rb
@@ -0,0 +1,7 @@
+log_level :info
+log_location STDOUT
+node_name 'maykelsw7t3400.us.corp'
+client_key 'client.pem'
+validation_client_name 'chef-validator'
+chef_server_url 'https://precise64'
+cache_type 'BasicFile'

3cd01ccd127cb0250c6724bde0b2d9e8fb0b64c3
mike suarez <mikemps@gmail.com>
2013-03-21 17:16:12 -0400

    adding create user to openstack

--- cookbooks/tests/recipes/application_server.rb
+++ cookbooks/tests/recipes/application_server.rb
@@ -3,6 +3,8 @@ include_recipe 'tests::default'
 TAGS = '-t @app_server'
 OUT = '-f pretty -f html -o c:\temp\infrastructure_results.html'
 
+ENV['windows/new_user_name'] = node[:windows][:new_user_name]
+
 execute 'Running tests' do
   command "cucumber #{node[:tests_directory]} #{TAGS} #{OUT}"
   cwd '/'
